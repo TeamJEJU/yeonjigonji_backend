7b2c5ae91f5ce63e92592e23d247ef76ad87ef77
kimminju <mindori1019@gmail.com>
2021-10-10 15:02:45 +0900

    initial commit

---
+++ src/main/java/com/JEJU/yeonjigonji_backend/YeonjigonjiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.JEJU.yeonjigonji_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeonjigonjiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeonjigonjiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JEJU/yeonjigonji_backend/YeonjigonjiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JEJU.yeonjigonji_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeonjigonjiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
